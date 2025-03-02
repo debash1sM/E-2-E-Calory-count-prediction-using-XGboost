cb70be4d618194d1ef7e3ea65752fcc2815dcbb4
“Debashis <debashis.deep205@gmail.com>
2025-03-03 01:00:17 +0530

    Setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='E2E Calory Prediction',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'xgboost',
+        'seaborn',
+        'matplotlib'
+    ],
+    author='Debashis Mondal',
+    author_email='debashis.deep205@gmail.com',
+    description='This project is for predicting calory count after workout',
+    url='https://github.com/debash1sM/E-2-E-Calory-count-prediction-using-XGboost',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
