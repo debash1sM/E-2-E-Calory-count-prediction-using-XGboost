66236e1d1a7cf90653a134efebab4c202112096b
“Debashis <debashis.deep205@gmail.com>
2025-03-07 00:27:19 +0530

    added dockerfile and docker compose

--- app.py
+++ app.py
@@ -4,16 +4,60 @@ from src.CaloryPrediction.logger import logging
 from src.CaloryPrediction.exception import customException
 import mysql.connector
 import numpy as np
+import os
 
-# Database configuration
-db_config = {
-    'user': 'root',
-    'password': 'mysql',
-    'host': 'localhost',
-    'database': 'calory_prediction'
-}
+# # Database configuration
+# db_config = {
+#     'user': os.getenv('MYSQL_USER', 'root'),
+#     'password': os.getenv('MYSQL_PASSWORD', 'mysql'),
+#     'host': os.getenv('MYSQL_HOST', 'db'),
+#     'database': os.getenv('MYSQL_DB', 'calory_prediction')
+# }
+# db_config = {
+#     'user': 'root',
+#     'password': 'mysql',
+#     'host': 'localhost',
+#     'database': 'calory_prediction'
+# }
 
 app = Flask(__name__)
+
+# def init_db():
+#     try:
+#         # conn = mysql.connector.connect(**db_config)
+#         # cursor = conn.cursor()
+#         # # Check if the table exists
+#         # cursor.execute("""
+#         # SELECT COUNT(*)
+#         # FROM information_schema.tables
+#         # WHERE table_name = 'predictions'
+#         # """)
+#         # if cursor.fetchone()[0] == 0:
+#         #     # Create the table if it does not exist
+#         #     create_table_query = """
+#         #     CREATE TABLE predictions (
+#         #         id INT AUTO_INCREMENT PRIMARY KEY,
+#         #         gender VARCHAR(10),
+#         #         age INT,
+#         #         height FLOAT,
+#         #         weight FLOAT,
+#         #         duration FLOAT,
+#         #         heart_rate FLOAT,
+#         #         body_temp FLOAT,
+#         #         prediction FLOAT,
+#         #         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#         #     );
+#         #     """
+#         #     cursor.execute(create_table_query)
+#         #     conn.commit()
+#         #     print("Table 'predictions' created in MySQL database.")
+#         # else:
+#         #     print("Table 'predictions' already exists in MySQL database.")
+#         # cursor.close()
+#         # conn.close()
+#         print("Database did not initialized.")
+#     except mysql.connector.Error as err:
+#         print(f"Error: {err}")
 
 @app.route('/')
 def home():
@@ -62,16 +106,17 @@ def predict():
 
         try:
             # Insert data into MySQL database
-            conn = mysql.connector.connect(**db_config)
-            cursor = conn.cursor()
-            insert_query = """
-            INSERT INTO predictions (gender, age, height, weight, duration, heart_rate, body_temp, prediction)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            """
-            cursor.execute(insert_query, (Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp, prediction_value))
-            conn.commit()
-            cursor.close()
-            conn.close()
+            # conn = mysql.connector.connect(**db_config)
+            # cursor = conn.cursor()
+            # insert_query = """
+            # INSERT INTO predictions (gender, age, height, weight, duration, heart_rate, body_temp, prediction)
+            # VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            # """
+            # cursor.execute(insert_query, (Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp, prediction_value))
+            # conn.commit()
+            # cursor.close()
+            # conn.close()
+            print("Data inserted into MySQL database.")
         except customException as e:
             logging.error(e)
         finally:
@@ -82,4 +127,5 @@ def predict():
         return render_template('index.html', prediction_text='An error occurred during prediction.')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #init_db()
+    app.run(debug=True, host='0.0.0.0')
